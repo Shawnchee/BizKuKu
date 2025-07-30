b1a41169968f16e8d81b73a1faffa1ac6ef54713
Shawn Chee <aykaychee2@gmail.com>
2025-07-30 11:43:30 +0800

    metadata change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BizzKuku",
+  title: "BizKuku",
   description: "Track sales, customers, and money easily. Perfect for small shops and stalls.",
+  icons: {
+    icon: "/logo.png"
+  }
 };
 
 export default function RootLayout({
